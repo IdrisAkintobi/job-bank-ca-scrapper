db7d87d865cc491323f0cfe8ceb92df177c65b06
Idris Akintobi Adejare <akintobiidris@gmail.com>
2024-04-20 22:41:02 +0100

    added email config and template

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -4,4 +4,10 @@ export const configValidationSchema = Joi.object({
     BASE_URL: Joi.string().uri().required(),
     LMIA_APPROVED_EMPLOYER_PAGE: Joi.string().uri().required(),
     LMIA_PENDING_EMPLOYER_PAGE: Joi.string().uri().required(),
+    EMAIL_HOST: Joi.string().required(),
+    EMAIL_PORT: Joi.number().required(),
+    EMAIL_SECURE: Joi.boolean().required(),
+    EMAIL_USER: Joi.string().required(),
+    EMAIL_PASSWORD: Joi.string().required(),
+    SENDER_NAME: Joi.string().required(),
 });
