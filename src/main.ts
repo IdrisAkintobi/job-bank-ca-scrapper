37b94036be909accf7b35936e8b06de563aeee84
Idris Akintobi Adejare <akintobiidris@gmail.com>
2024-04-20 22:19:11 +0100

    refactored main and updated util class

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,62 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
+import { spawnSync } from 'node:child_process';
 import { AppModule } from './app.module.js';
 import { JobQueryTypeEnum } from './domain/enum/job-query.enum.js';
+import { emailJobTitlesHandler } from './handlers/email-job-titles.handler.js';
 import { queryTypeHandler } from './handlers/query-type.handler.js';
 import { searchPagesHandler } from './handlers/search-pages.handler.js';
 import { searchQueryHandler } from './handlers/search-query.handler.js';
-import { ScraperService } from './scraper/scraper.service.js';
+import { EmailService } from './services/email.service.js';
+import { ScraperService } from './services/scraper.service.js';
 
-// Define CLI commands
 async function startScraper() {
     try {
+        // Ensure playwright is installed before running the script
+        const result = spawnSync('npx', ['playwright', 'install'], { stdio: 'inherit' });
+        if (result.error || result.status !== 0) {
+            console.error('Failed to run "npx playwright install"');
+            process.exit(1);
+        }
+
         const app = await NestFactory.createApplicationContext(AppModule);
         const scraperService = app.get(ScraperService);
         const configService = app.get(ConfigService);
+        const emailService = app.get(EmailService);
 
         const LMIAApprovedEmployerPage = configService.get('LMIA_APPROVED_EMPLOYER_PAGE');
         const LMIAPendingEmployerPage = configService.get('LMIA_PENDING_EMPLOYER_PAGE');
 
         const { selectedQueryType } = await queryTypeHandler();
-        const { noOfPages } = await searchPagesHandler();
 
         switch (selectedQueryType) {
             case JobQueryTypeEnum.LMIA_APPROVED:
-                await scraperService.scrapeJobSearchResultPage(LMIAApprovedEmployerPage, noOfPages);
+                const { noOfPages: approvedPages } = await searchPagesHandler();
+                await scraperService.scrapeJobSearchResultPage(
+                    LMIAApprovedEmployerPage,
+                    approvedPages,
+                );
                 break;
             case JobQueryTypeEnum.LMIA_PENDING:
-                await scraperService.scrapeJobSearchResultPage(LMIAPendingEmployerPage, noOfPages);
+                const { noOfPages: pendingPages } = await searchPagesHandler();
+                await scraperService.scrapeJobSearchResultPage(
+                    LMIAPendingEmployerPage,
+                    pendingPages,
+                );
                 break;
-            default:
+            case JobQueryTypeEnum.ALL_JOBS:
                 const { jobTitle, jobLocation } = await searchQueryHandler();
+                const { noOfPages } = await searchPagesHandler();
                 await scraperService.scrapeJobBank(jobTitle, jobLocation, noOfPages);
                 break;
+            case JobQueryTypeEnum.SEND_EMAIL:
+                const { jobTitlesArray, noOfJobsToEmail } = await emailJobTitlesHandler();
+                await emailService.sendEmailToEmployers(jobTitlesArray, noOfJobsToEmail);
+                break;
         }
-        app.close();
+
+        await app.close();
         process.exit(0);
     } catch (error) {
         console.error(error);
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,5 @@
 import { Presets, SingleBar } from 'cli-progress';
+import { URL } from 'node:url';
 
 import { JobSearchResult } from '../domain/interface.job-search-result.js';
 
@@ -37,5 +38,59 @@ export class Utils {
                 data.map(item => [item.email || Math.random().toString().substring(2, 8), item]),
             ).values(),
         ];
+    }
+
+    public static convertLocation(location: string) {
+        const provinceMap = {
+            AB: 'Alberta',
+            BC: 'British Columbia',
+            MB: 'Manitoba',
+            NB: 'New Brunswick',
+            NL: 'Newfoundland and Labrador',
+            NS: 'Nova Scotia',
+            NT: 'Northwest Territories',
+            NU: 'Nunavut',
+            ON: 'Ontario',
+            PE: 'Prince Edward Island',
+            QC: 'Quebec',
+            SK: 'Saskatchewan',
+            YT: 'Yukon',
+        };
+
+        const match = location.match(/^(.+)\s\((\w{2})\)$/);
+        if (!match) {
+            return location;
+        }
+
+        const city = match[1];
+        const provinceAbbr = match[2];
+
+        const province = provinceMap[provinceAbbr];
+        if (!province) {
+            return location;
+        }
+
+        return `${city}, ${province}`;
+    }
+
+    public static capitalizeJobTitle(jobTitle: string): string {
+        return jobTitle.replace(/\b\w/g, char => char.toUpperCase());
+    }
+
+    public static async sleep(ms: number): Promise<void> {
+        console.log(`Sleeping for ${ms / 1000} seconds`);
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    public static extractJobId(urlString: string): string | null {
+        try {
+            const url = new URL(urlString);
+            const parts = url.pathname.split('/');
+            const jobId = parts[parts.length - 1];
+            return jobId.split(';')[0]; // Exclude any session data after the job ID
+        } catch (error) {
+            console.error('Invalid URL:', urlString);
+            return null;
+        }
     }
 }
